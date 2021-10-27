a73920f7bcf136e3e96fd02c87df7b9fa711176d
FidelFuentes <fidelfuentes.fullstack@gmail.com>
2021-10-27 09:44:28 -0300

    homebody

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,28 +1,37 @@
 import React, { Component } from "react";
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
-import '../styles/global.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "../styles/global.css";
 import Layout from "../conteiners/Layout";
 import Login from "../conteiners/login";
 import RecoveryPassword from "../conteiners/RecoveryPassword";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import SignUp from "../conteiners/SignUp";
+import CreateAccount from "../conteiners/CreateAccount";
+import EditAccount from "../conteiners/EditAccount";
+import HomeBody from "../conteiners/HomeBody";
 
-const App = ()=>{
-    return (
-        <BrowserRouter>
-        <Layout>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
         <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/login" component={Login}></Route>
-                <Route exact path="/recovery-password" component={RecoveryPassword}></Route>
-                <Route path="*" component={NotFound}></Route>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route
+            exact
+            path="/recovery-password"
+            component={RecoveryPassword}
+          ></Route>
+          <Route exact path="/signup" component={SignUp}></Route>
+          <Route exact path="/createaccount" component={CreateAccount}></Route>
+          <Route exact path="/editaccount" component={EditAccount}></Route>
+          <Route exact path="/homebody" component={HomeBody}></Route>
+          <Route path="*" component={NotFound}></Route>
         </Switch>
-        </Layout>
-        </BrowserRouter>
-        
-    );
-}
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
-
